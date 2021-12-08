2dfc434da019cceab3bc9cb80ea92fc17559e656
Rekha <59589300+therekharaju@users.noreply.github.com>
2021-12-08 18:16:47 -0500

    changed crime_district_analysis to run from main

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import arrests.arrestsRunner as arrestsRunner
 import domestic.domesticRunner as domesticRunner
+import Azal.crime_district_analysis as crime_district_analysis
 
-domesticRunner = domesticRunner.domesticRunner()
-domesticRunner.run()
+# Analysis of domestic cases - results shown with plt.show()
+dr = domesticRunner.domesticRunner()
+dr.run()
+
+# Analysis of crime per district - results shown with plt.show()
+cda = crime_district_analysis.crime_district_analysis()
+cda.run()
 
 # I've turned off the generation of matplotlib charts and output files to speed things up.
 # And just like, not play around in memory for no reason.
 # To test those, you can set the parameters in the constructor to True.
 # If false, the program will still print some summary data to show that it's working.
-ar = arrestsRunner.arrestsRunner(newgraphs=True, newOutliers=True)
+ar = arrestsRunner.arrestsRunner(newgraphs=False, newOutliers=False)
 ar.run()
